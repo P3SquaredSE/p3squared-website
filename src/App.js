aa054e97374de9ea7f5c84a63487af9fea56aa36
stephen bandi <stephenbandi342@gmail.com>
2025-11-25 22:39:25 -0500

    Wire homepage as main app layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>P3Squared React Starter</h1>
-    </div>
-  );
+  return 
+    return <Home />;
+  
 }
 
 export default App;
