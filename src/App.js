20185ba3fde6957ded47cb44e4eae5c405370883
stephen bandi <stephenbandi342@gmail.com>
2026-03-01 00:25:29 -0500

    OurPhilisophy page update 1.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Philosophy from './pages/Philosphy';
+import './App.css';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/philosophy" element={<Philosophy />} />
+    </Routes>
+  );
 }
 
 export default App;
