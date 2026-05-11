63c8b4ff51b2b0ea3ad912c45f6797dcdc605c76
stephen bandi <stephenbandi342@gmail.com>
2026-05-11 13:55:15 -0400

    changes to cost page and app.js to add route for the changed page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Philosophy from './pages/Philosophy';
+import Costs from './pages/Costs';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/philosophy" element={<Philosophy />} />
+      <Route path="/costs" element={<Costs />} />
     </Routes>
   );
 }
