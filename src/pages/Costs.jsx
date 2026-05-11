c157f24ff2594393bb271927e36aa0bc95077db4
stephen bandi <stephenbandi342@gmail.com>
2026-05-11 13:36:45 -0400

    changes

--- src/pages/Costs.jsx
+++ src/pages/Costs.jsx
@@ -1,4 +1,4 @@
-
+import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
 import Capabilities from "../components/Capabilities"; 
 import ContactModal from "../components/ContactModal";
