56cae3677accb91a02927625b7dcf15aad5ccaf8
stephen bandi <stephenbandi342@gmail.com>
2026-05-20 14:50:11 -0400

    Cost page fix 1.52

--- src/pages/Costs.jsx
+++ src/pages/Costs.jsx
@@ -56,9 +56,9 @@ export default function Costs() {
         </section>
 
         {/* Supply Overview */}
-        <Section className="container mx-auto px-4 md:px-6 max-w=7xl">
+        <section className="container mx-auto px-4 md:px-6 max-w=7xl">
           <SupplyOverview />
-        </Section>
+        </section>
 
         {/* Labor Calculator */}
         <section className="relative container mx-auto max-w-7xl px-4 md:px-6 pb-24"
