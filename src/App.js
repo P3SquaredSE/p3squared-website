498cf2b3a27a4c94ef92846d75855e8a04c7575e
hggould-ai <hggould@goshen.edu>
2026-05-22 03:03:50 -0400

    FInally fixed blog and team pages and added nackground glow to both

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Philosophy from './pages/Philosophy';
-import Costs from './pages/Costs';
+import Costs from './pages/Costs'; 
+import Blog from './pages/Blog'; 
+import Team from './pages/Team.jsx';
 import './App.css';
+
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/philosophy" element={<Philosophy />} />
-      <Route path="/costs" element={<Costs />} />
+      <Route path="/costs" element={<Costs />} /> 
+      <Route path="/Blog" element={<Blog/>} /> 
+      <Route path="/Team" element={<Team/>} />
     </Routes>
   );
 }
