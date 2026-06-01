a177aa77ccfbbb4f4d73c632ca90958c2596b614
stephen bandi <stephenbandi342@gmail.com>
2026-06-01 11:15:29 -0400

    Improve text title on mobile

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -63,9 +63,9 @@ export default function HeroSection() {
                 WELCOME TO
               </p>
 
-              <h1 className="mt-3 text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
+              <h1 className="mt-4 text-[2rem] sm:text-4xl font-bold leading-[1.25] tracking-tight">
                 Transforming Rural Healthcare
-                <br />
+                <span className="hidden sm:block"ss />
                 with AI + Human Oversight
               </h1>
             </div>
